5710d240863749ee6e048d297b919be12560e793
tytongyu <tytongyu@163.com>
2022-08-24 20:13:00 +0800

    hw2 version1

--- hw2/hw2/Percolation.java
+++ hw2/hw2/Percolation.java
@@ -3,5 +3,95 @@ package hw2;
 import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
 public class Percolation {
+    private WeightedQuickUnionUF simulate;
+    private boolean[] isopem;
+    private int gridsize;
+    private int openNum;
 
+    public Percolation(int N) {
+
+        if (N <= 0) {
+            throw new IllegalArgumentException("N cannot < 0");
+        }
+
+        simulate = new WeightedQuickUnionUF(N * N);
+        gridsize = N;
+        isopem = new boolean[N * N];
+        for (Boolean a : isopem) {
+            a = false;
+        }
+        openNum = 0;
+    }
+
+    public void open(int row, int col) {
+        if (!isopem[row * gridsize + col]) {
+            isopem[row * gridsize + col] = true;
+            openNum++;
+        }
+        int[] a = {-1, 1, -gridsize, +gridsize};
+        for (int t : a) {
+            if (row * gridsize + col + t < 0 || row * gridsize + col + t > gridsize * gridsize -1) {
+                continue;
+            }
+            if ((row * gridsize + col + t) % gridsize == 0 && (row * gridsize + col) % gridsize == gridsize - 1) {
+                continue;
+            }
+            if ((row * gridsize + col) % gridsize == 0 && (row * gridsize + col + t) % gridsize == gridsize - 1) {
+                continue;
+            }
+            if (isopem[row * gridsize + col + t]) {
+                simulate.union(row * gridsize + col, row * gridsize + col + t);
+            }
+        }
+    }
+
+    public boolean isOpen(int row, int col) {
+        return isopem[row * gridsize + col];
+    }
+
+    public boolean isFull(int row, int col) {
+        for (int i = 0; i < gridsize; i++) {
+            if (simulate.find(row * gridsize + col) == simulate.find(i) && isopem[row * gridsize + col]) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public int numberOfOpenSites() {
+        return openNum;
+    }
+
+    public boolean percolates() {
+        for (int i = 0; i < gridsize; i++) {
+            if (isFull(gridsize - 1, i)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        Percolation a = new Percolation(6);
+        a.open(0,5);
+        a.open(1,5);
+        a.open(2,5);
+        a.open(3,5);
+        a.open(4,5);
+        a.open(4,4);
+        a.open(3,3);
+        a.open(2,3);
+        a.open(1,3);
+        a.open(1,2);
+        a.open(1,1);
+        a.open(1,0);
+        a.open(2,0);
+        System.out.println(a.isOpen(1,1));
+        System.out.println(a.isOpen(2,1));
+        System.out.println(a.isFull(1,1));
+        System.out.println(a.isFull(2,1));
+        a.open(2,1);
+        System.out.println(a.isOpen(2,1));
+        System.out.println(a.isFull(2,1));
+    }
 }
--- hw2/hw2/PercolationStats.java
+++ hw2/hw2/PercolationStats.java
@@ -1,5 +1,63 @@
 package hw2;
 
-public class PercolationStats {
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 
+public class PercolationStats {
+    private Percolation simulate;
+    private int simulateNum;
+    private int[] percolationRusult;
+    private int gridsize;
+    private Random r1;
+
+    public PercolationStats(int N, int T, PercolationFactory pf) {
+        simulateNum = T;
+        if (N <= 0 || T <= 0) {
+            throw new IllegalArgumentException("Illegal Values");
+        }
+        simulate = pf.make(N);
+        gridsize = N;
+        percolationRusult = new int[T];
+        int j = 0;
+        while (j < T) {
+            List<Integer> l1 = new ArrayList<>();
+            for (int i = 0; i < N * N; i++) {
+                l1.add(i);
+            }
+            int randomNum;
+            while(!simulate.percolates()) {
+                randomNum = r1.nextInt(l1.size() - 1);
+                simulate.open(0, l1.get(randomNum));
+                l1.remove(randomNum);
+            }
+            percolationRusult[j] = simulate.numberOfOpenSites();
+            j++;
+        }
+    }
+    // perform T independent experiments on an N-by-N grid
+    public double mean() {
+        int percolationSum = 0;
+        for (int a : percolationRusult) {
+            percolationSum += a;
+        }
+        return percolationSum/simulateNum;
+    }
+    // sample mean of percolation threshold
+    public double stddev() {
+        int percolationSquareSum = 0;
+        for (int a : percolationRusult) {
+            percolationSquareSum += (a - mean()) * (a - mean());
+        }
+        return Math.sqrt(percolationSquareSum/(simulateNum - 1));
+    }
+    // sample standard deviation of percolation threshold
+    public double confidenceLow() {
+        return mean() - 1.96 * stddev() / Math.sqrt(simulateNum);
+    }
+    // low endpoint of 95% confidence interval
+    public double confidenceHigh() {
+        return mean() + 1.96 * stddev() / Math.sqrt(simulateNum);
+    }
+    // high endpoint of 95% confidence interval
 }
