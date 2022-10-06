9a38949903a6a12117a228b05b1ddec04e08a807
tytongyu <tytongyu@163.com>
2022-10-06 22:30:53 +0800

    lab12

--- lab12/MergeSort.java
+++ lab12/MergeSort.java
@@ -71,6 +71,9 @@ public class MergeSort {
     /** Returns a Queue that contains the given items sorted from least to greatest. */
     public static <Item extends Comparable> Queue<Item> mergeSort(
             Queue<Item> items) {
+        if (items.size() <= 1) {
+            return items;
+        }
         Queue<Queue<Item>> singleItemQueues = makeSingleItemQueues(items);
         while (singleItemQueues.size() != 1) {
             singleItemQueues.enqueue(mergeSortedQueues(singleItemQueues.dequeue(), singleItemQueues.dequeue()));
