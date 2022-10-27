89f6b32b2a55dad66b486895b57e82fb0cf279ec
tytongyu <tytongyu@163.com>
2022-10-27 22:08:35 +0800

    hw5

--- hw5/SeamCarver.java
+++ hw5/SeamCarver.java
@@ -3,11 +3,11 @@ import edu.princeton.cs.algs4.Picture;
 public class SeamCarver {
 
     private edu.princeton.cs.algs4.Picture picture;
-    public int width;
-    public int height;
+    private int width;
+    private int height;
 
     public SeamCarver(Picture picture) {
-        this.picture = picture;
+        this.picture = new Picture(picture);
         this.width = picture.width();
         this.height = picture.height();
     }
@@ -20,21 +20,27 @@ public class SeamCarver {
         return this.width;
     }
     public int height() {
-        return this.width;
+        return this.height;
     }
 
     // energy of pixel at column x and row y
     public double energy(int x, int y) {
-        if (x < 0 || x >=width || y < 0 || y >=height) {
+        if (x < 0 || x >= width || y < 0 || y >= height) {
             throw new IndexOutOfBoundsException();
         }
-        double dx21 = Math.pow(picture.get((x - 1 + width) % width, y).getRed() - picture.get((x + 1) % width, y).getRed(), 2);
-        double dx22 = Math.pow(picture.get((x - 1 + width) % width, y).getGreen() - picture.get((x + 1) % width, y).getGreen(), 2);
-        double dx23 = Math.pow(picture.get((x - 1 + width) % width, y).getBlue() - picture.get((x + 1) % width, y).getBlue(), 2);
+        double dx21 = Math.pow(picture.get((x - 1 + width) % width, y).getRed()
+                - picture.get((x + 1) % width, y).getRed(), 2);
+        double dx22 = Math.pow(picture.get((x - 1 + width) % width, y).getGreen()
+                - picture.get((x + 1) % width, y).getGreen(), 2);
+        double dx23 = Math.pow(picture.get((x - 1 + width) % width, y).getBlue()
+                - picture.get((x + 1) % width, y).getBlue(), 2);
         double dx2 = dx21 + dx22 + dx23;
-        double dy21 = Math.pow(picture.get(x, (y - 1 + height) % height).getRed() - picture.get(x, (y + 1) % height).getRed(), 2);
-        double dy22 = Math.pow(picture.get(x, (y - 1 + height) % height).getGreen() - picture.get(x, (y + 1) % height).getGreen(), 2);
-        double dy23 = Math.pow(picture.get(x, (y - 1 + height) % height).getBlue() - picture.get(x, (y + 1) % height).getBlue(), 2);
+        double dy21 = Math.pow(picture.get(x, (y - 1 + height) % height).getRed()
+                - picture.get(x, (y + 1) % height).getRed(), 2);
+        double dy22 = Math.pow(picture.get(x, (y - 1 + height) % height).getGreen()
+                - picture.get(x, (y + 1) % height).getGreen(), 2);
+        double dy23 = Math.pow(picture.get(x, (y - 1 + height) % height).getBlue()
+                - picture.get(x, (y + 1) % height).getBlue(), 2);
         double dy2 = dy21 + dy22 + dy23;
         return dx2 + dy2;
     }
@@ -78,7 +84,7 @@ public class SeamCarver {
         }
         int shortestPath = paths[bottomx][height - 1];
         int[] res = new int[height];
-        res[height - 1] =bottomx;
+        res[height - 1] = bottomx;
         for (int i = height -2; i > -1; i--) {
             shortestPath -= energy(bottomx, i + 1);
             if (bottomx - 1 >= 0) {
@@ -91,7 +97,7 @@ public class SeamCarver {
                     bottomx++;
                 }
             }
-            res[i] =bottomx;
+            res[i] = bottomx;
         }
         return res;
     }
